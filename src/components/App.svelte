f91be8e43bbf32312447633a9171274b38d62b32
Timon Weber <hi@timonweber.com>
2020-05-02 18:00:19 +0200

    Implement Paper component

---
+++ src/components/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import 'minireset.css';
+  import '@openfonts/ibm-plex-sans_latin';
+  import '@openfonts/ibm-plex-mono_latin';
+
+  import Paper from './Paper.svelte';
+</script>
+
+<style>
+  :global(body) {
+    padding: 0.25rem;
+
+    color: white;
+    background-color: black;
+  }
+</style>
+
+<Paper name="A0" width={841} height={1189} />
---
+++ src/components/Paper.svelte
@@ -0,0 +1,42 @@
+<script>
+  export let orientation = 'portrait';
+  export let name, width, height;
+</script>
+
+<style>
+  .root {
+    position: relative;
+
+    width: 100%;
+    height: 0;
+
+    color: black;
+    border: 0.25rem solid black;
+    background-color: white;
+  }
+
+  .root.portrait {
+    padding-top: calc(141.42135624% - 0.5rem);
+  }
+
+  .root.landscape {
+    padding-top: calc(70.710678119% - 0.5rem);
+  }
+
+  .content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+
+    transform: translate(-50%, -50%);
+    text-align: center;
+    white-space: nowrap;
+  }
+</style>
+
+<section class="root {orientation}">
+  <div class="content">
+    <h2>{name}</h2>
+    <p>{width} &times; {height} mm</p>
+  </div>
+</section>
