22de21615f1d95db6490766378a20eed52bf4613
Timon Weber <hi@timonweber.com>
2020-05-05 21:27:58 +0200

    Add header

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -3,6 +3,7 @@
   import '@openfonts/ibm-plex-sans_latin';
   import '@openfonts/ibm-plex-mono_latin';
 
+  import Text from './Text.svelte';
   import RecursiveLayout from './RecursiveLayout.svelte';
   import Paper from './Paper.svelte';
 
@@ -16,7 +17,18 @@
     color: white;
     background-color: black;
   }
+
+  header {
+    position: absolute;
+
+    padding: 0.25rem;
+  }
 </style>
+
+<header>
+  <Text>wtform.at</Text>
+  <Text>by @timonweber</Text>
+</header>
 
 <RecursiveLayout column payload={paperSizes.a.slice(1)} />
 <Paper {...paperSizes.a[0]} />
