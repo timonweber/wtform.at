ac2f697b1d8fae8a03b208fa6a1cae6d62e939ac
Timon Weber <hi@timonweber.com>
2020-05-10 14:10:00 +0200

    Unify leading of typography components

--- src/components/Text.svelte
+++ src/components/Text.svelte
@@ -2,6 +2,7 @@
   p {
     font-family: 'IBM Plex Mono';
     font-size: 0.875rem;
+    line-height: 1rem;
   }
 </style>
 
--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -3,6 +3,7 @@
     font-family: 'IBM Plex Sans';
     font-size: 2rem;
     font-weight: 700;
+    line-height: 2.5rem;
   }
 </style>
 
